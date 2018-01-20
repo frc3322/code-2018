566903436a0a16b7e47ff8e4196773fc6da19bdc
Andriy Massimilla <andriymassimilla@gmail.com>
2018-01-20 14:10:51 -0500

    Clean up RobotMap

--- src/frc/team3322/RobotMap.java
+++ src/frc/team3322/RobotMap.java
@@ -22,9 +22,4 @@ public class RobotMap
     public static final int RIGHT_BACK_MOTOR = 4;
     public static final int RIGHT_MIDDLE_MOTOR = 5;
     public static final int RIGHT_FRONT_MOTOR = 6;
-
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
 }
