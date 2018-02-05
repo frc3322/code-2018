02ac52fb97dc73487cfbdfc1261b4dc520636f64
Andriy Massimilla <andriymassimilla@gmail.com>
2018-02-05 07:48:21 -0500

    Update RobotMap for P1 board

--- src/frc/team3322/RobotMap.java
+++ src/frc/team3322/RobotMap.java
@@ -20,22 +20,20 @@ public class RobotMap
      */
     public static class CAN {
         // Drivetrain
-        public static final int LEFT_BACK_MOTOR = 9;
-        //public static final int LEFT_MIDDLE_MOTOR = 0;
-        //public static final int LEFT_FRONT_MOTOR = 11;
-        public static final int RIGHT_BACK_MOTOR = 4;
-        //public static final int RIGHT_MIDDLE_MOTOR = 0;
-        //public static final int RIGHT_FRONT_MOTOR = 5;
+        public static final int LEFT_BACK_MOTOR = 11;
+        public static final int LEFT_FRONT_MOTOR = 12;
+        public static final int RIGHT_BACK_MOTOR = 5;
+        public static final int RIGHT_FRONT_MOTOR = 6;
 
         // Elevator
-        public static final int ELEVATOR_MOTOR_1 = 8;
-        public static final int ELEVATOR_MOTOR_2 = 10;
+        public static final int ELEVATOR_MOTOR_1 = 1;
+        public static final int ELEVATOR_MOTOR_2 = 2;
 
-        // Cube Intake
-        public static final int LEFT_INTAKE = 12;
-        public static final int RIGHT_INTAKE = 13;
-        public static final int LEFT_ARM = 14;
-        public static final int RIGHT_ARM = 15;
+        // Arms
+        public static final int LEFT_ARM = 3;
+        public static final int RIGHT_ARM = 7;
+        public static final int LEFT_INTAKE = 8;
+        public static final int RIGHT_INTAKE = 9;
     }
 
     /**
