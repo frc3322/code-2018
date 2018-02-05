a932dda54ccff205f3d7aafd77b118038e9962fa
Andriy Massimilla <andriymassimilla@gmail.com>
2018-02-05 13:08:36 -0500

    Simplify ShiftDrivetrain

--- src/frc/team3322/commands/ShiftDrivetrain.java
+++ src/frc/team3322/commands/ShiftDrivetrain.java
@@ -1,35 +1,13 @@
 package frc.team3322.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 
 import static frc.team3322.Robot.drivetrain;
 
 
-public class ShiftDrivetrain extends Command {
-    public ShiftDrivetrain() {
-    }
-
-    @Override
-    protected void initialize() {
-    }
-
+public class ShiftDrivetrain extends InstantCommand {
     @Override
     protected void execute() {
         drivetrain.toggleShifter();
-    }
-
-    @Override
-    protected boolean isFinished() {
-        return true;
-    }
-
-    @Override
-    protected void end() {
-
-    }
-
-    @Override
-    protected void interrupted() {
-        super.interrupted();
     }
 }
